5c4e65243ba9ddd0ee44303f1c78791987b51521
Fernando Arthur <fernando.atr@outlook.com>
2023-03-12 17:29:19 -0300

    new commit

--- src/adheart/adheart.controller.ts
+++ src/adheart/adheart.controller.ts
@@ -18,7 +18,7 @@ export class AdheartController {
 
   @UseGuards(AccessTokenGuard, PermissionGuard(['get_adheart']))
   @Post()
-  @Throttle(200, 60 * 60 * 24)
+  @Throttle(200, 60)
   async getAdheart(@Query() query: { [key: string]: string }, @Req() req: any) {
     return this.adheartService.getPage(req._parsedUrl.query);
   }
